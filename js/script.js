2c63f8c37673824292bcdac6f81bb261352facf7
GabrielSgaria <gabrielsgaria2@gmail.com>
2023-11-02 15:17:04 -0300

    Adicionando Clientes e referenciando na pagina

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
@@ -8,24 +6,27 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
 // Formspree
 
-let form = document.getElementById('form-orc');
-const obgName = document.getElementById('obg-name');
-const obgEmail = document.getElementById('obg-email')
-const obgTelefone = document.getElementById('obg-telefone')
-const obgMensagem = document.getElementById('obg-mensagem')
-
+let form = document.getElementById("form-orc");
+const obgName = document.getElementById("obg-name");
+const obgEmail = document.getElementById("obg-email");
+const obgTelefone = document.getElementById("obg-telefone");
+const obgMensagem = document.getElementById("obg-mensagem");
+const formulario = document.getElementById("form-orc");
+const btnEnviar = document.getElementById("btn-enviar");
 
 function enviarForm(e) {
   e.preventDefault();
+  btnEnviar.disabled = true;
+  btnEnviar.innerText = "Enviando...";
 
-  let status = document.querySelector('.my-form-status');
+  let status = document.querySelector(".my-form-status");
   let data = new FormData(e.target);
 
   const name = data.get("name");
@@ -33,62 +34,75 @@ function enviarForm(e) {
   const telefone = data.get("telefone");
   const mensagem = data.get("mensagem");
 
-  let erro = false
+  let erro = false;
 
-  obgName.innerHTML = ""
-  obgEmail.innerHTML = ""
-  obgTelefone.innerHTML = ""
-  obgMensagem.innerHTML = ""
+  obgName.innerHTML = "";
+  obgEmail.innerHTML = "";
+  obgTelefone.innerHTML = "";
+  obgMensagem.innerHTML = "";
 
-  if (name === '') {
-    obgName.innerHTML = "Campo obrigatorio! "
+  if (name === "") {
+    obgName.innerHTML = "Campo obrigatorio! ";
+    erro = true;
+  }
+  if (email === "") {
+    obgEmail.innerHTML = "Campo obrigatorio!";
+    erro = true;
+  }
+  if (telefone === "") {
+    obgTelefone.innerHTML = "Campo obrigatorio!";
+    erro = true;
+  }
+  if (mensagem === "") {
+    obgMensagem.innerHTML = "Campo obrigatorio!";
+    erro = true;
+  }
+  if (erro) {
+    btnEnviar.disabled = false;
+    btnEnviar.innerText = "Enviar";
+  }
+  if (erro) return;
 
-    erro = true
-  }
-  if (email === '') {
-    obgEmail.innerHTML = "Campo obrigatorio!"
-    erro = true
-  }
-  if (telefone === '') {
-    obgTelefone.innerHTML = "Campo obrigatorio!"
-    erro = true
-  }
-  if (mensagem === '') {
-    obgMensagem.innerHTML = "Campo obrigatorio!"
-    erro = true
-  }
-  if (erro) return
-
-  erro = false
-  obgName.innerHTML = ""
-  obgEmail.innerHTML = ""
-  obgTelefone.innerHTML = ""
-  obgMensagem.innerHTML = ""
+  erro = false;
+  obgName.innerHTML = "";
+  obgEmail.innerHTML = "";
+  obgTelefone.innerHTML = "";
+  obgMensagem.innerHTML = "";
 
   fetch("https://formspree.io/f/xaygvrde", {
     method: "POST",
     headers: {
-      "Accept": "application/json"
+      Accept: "application/json",
     },
-    body: data
-  }).then((response) => {
-    if (Object.hasOwn(data, 'errors')) {
-      status.innerHTML = "O orçamento não foi enviado !"
-      status.classList.add("form-error")
-      console.error(data["errors"].map(error => error["message"]).join(", "));
-    } else {
-      status.innerHTML = "Seu orçamento foi enviado com sucesso!"
-      form.reset();
-      status.classList.add("form-success");
-      setTimeout(() => {
-        status.innerHTML = ""
-      }, 4000)
-    }
-  }).catch((error) => {
-    status.innerHTML = "O orçamento não foi enviado !"
-    status.classList.add("form-error")
-    console.error(error);
+    body: data,
   })
+    .then((response) => {
+      if (Object.hasOwn(data, "errors")) {
+        status.innerHTML = "O orçamento não foi enviado !";
+        status.classList.add("form-error");
+        btnEnviar.disabled = false;
+        btnEnviar.innerText = "Enviar";
+        console.error(
+          data["errors"].map((error) => error["message"]).join(", ")
+        );
+      } else {
+        btnEnviar.disabled = false;
+        btnEnviar.innerText = "Enviar";
+        status.innerHTML = "Seu orçamento foi enviado com sucesso!";
+        form.reset();
+        status.classList.add("form-success");
+        setTimeout(() => {
+          status.innerHTML = "";
+        }, 4000);
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "O orçamento não foi enviado !";
+      status.classList.add("form-error");
+      console.error(error);
+      btnEnviar.disabled = false;
+      btnEnviar.innerText = "Enviar";
+    });
 }
 
 form.addEventListener("submit", enviarForm);
